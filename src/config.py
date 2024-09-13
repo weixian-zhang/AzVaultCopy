2fde674a0b2763905d5dda840e15840ee49165e1
wxz <weixianzhang@outlook.com>
2024-09-13 22:22:40 +0800

    feat: Supports cmdline arguments input

--- src/config.py
+++ src/config.py
@@ -13,15 +13,18 @@ load_dotenv()
 # az account get-access-token --scope https://vault.azure.net/.default --query "accessToken"
 class Config:
 
-    def __init__(self, src_token='', dest_token='') -> None:
+    def __init__(self,src_vault_name, dest_vault_name, 
+                 src_token='', dest_token='', export_dir='', 
+                 export_only=False, no_import_if_dest_exist=False) -> None:
+        
         self.src_token = os.environ.get('src_token') if not src_token else src_token
         self.dest_token = os.environ.get('dest_token') if not dest_token else dest_token
-        self.src_vault_name = 'akv-gccs'
-        self.dest_vault_name = 'akv-temp-3'
-        self.ignore_import_if_exist = True
-        self.export_only = False
-        self.out_dir = ''
-
+        self.src_vault_name = src_vault_name
+        self.dest_vault_name = dest_vault_name
+        self.export_dir = export_dir
+        self.no_import_if_dest_exist = no_import_if_dest_exist
+        self.export_only = export_only
+        
         self.source_azure_cred = ExistingTokenCredential(self.src_token)
         self.dest_azure_cred =  ExistingTokenCredential(self.dest_token)
 
