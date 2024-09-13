8aa0bf2c9e6d36790325f4c2eeb9dc290f5c107b
wxz <weixianzhang@outlook.com>
2024-09-13 22:22:40 +0800

    feat: Supports cmdline arguments input

--- src/azvc.py
+++ src/azvc.py
@@ -10,15 +10,33 @@ from log import log
 # package cmdline app publish to pypi
 # https://blog.thesourcepedia.org/build-cli-app-in-python-with-click-and-publish-to-pypi#heading-prepare-for-package
 
-def run():
+@click.command()
+@click.option('--src_vault', '-sv', default='', help='source vault name')
+@click.option('--dest_vault', '-dv', default='', help='destination vault name')
+@click.option('--src_token', '-st', default='', help='''access token of source Entra Tenant to access source vault.\n
+              az login --tenant {tenant id}\n
+              az account get-access-token --scope https://vault.azure.net/.default --query "accessToken" 
+              ''')
+@click.option('--dest_token', '-dt', default='', help='''access token of destination Entra Tenant to access source vault.\n
+              az login --tenant {tenant id}\n
+              az account get-access-token --scope https://vault.azure.net/.default --query "accessToken" 
+              ''')
+@click.option('--export_dir', '-ed', default='', help='certs and secrets are save to this directory while importing to dest vault')
+@click.option('--export_only', '-eo', is_flag=True, help='all certs and secrets are save to local drive, WITHOUT importing to dest vault')
+@click.option('--no_import_if_dest_exist', '-ii', is_flag=True, help='''any cert or secret with same name at dest vault will not be imported\n
+              * When importing an object with the same name, vault will create a new version.
+              ''')
+def run(src_vault, dest_vault, src_token, dest_token, export_dir='',export_only=False, no_import_if_dest_exist=False):
     try:
-         config = Config()
+         config = Config(src_vault, dest_vault, src_token, dest_token, export_dir, export_only, no_import_if_dest_exist)
          ei = ExportImporter(config)
          ei.run()
     except Exception as e:
             log.err(e)
 
+def main(*args):
+    run(*args)
 
 
 if __name__ == '__main__':
-    run()
+    main()
